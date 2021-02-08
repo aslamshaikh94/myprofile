ab903d5c536dda473ab26ee0671542a7f8758d1f
Aslam Ahmed <highflyingaslam@gmail.com>
2021-02-08 21:17:27 +0530

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,4 +18,4 @@ export const MONTH_SHORT_NAME = [
 ]
 
 export const DEFAULT_PROFILE_URL =
-  'https://firebasestorage.googleapis.com/v0/b/portfolio-64291.appspot.com/o/portfolio.jpg?alt=media&token=53119885-1fa3-4af7-b633-0eeeb8f713bd'
+  'https://res.cloudinary.com/dauiamqjl/image/upload/v1612799071/portfolio_feakwi.jpg'
